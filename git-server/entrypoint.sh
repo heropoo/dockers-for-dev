9cd37af7b9b1ae96c4ccfb6ad78d8a9732a1d53e
heropoo <aiyouyou1000@163.com>
2018-06-07 16:07:21 +0800

    git-server

--- git-server/entrypoint.sh
+++ git-server/entrypoint.sh
@@ -8,5 +8,8 @@ if [ ! -z ${ROOT_PASSWORD} ] && [ "${ROOT_PASSWORD}" != "123456" ]; then
     echo "root:${ROOT_PASSWORD}" | chpasswd
 fi
 
+chown -R git:git /home/git 
+chmod  600 /home/git/.ssh/authorized_keys 
+
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
